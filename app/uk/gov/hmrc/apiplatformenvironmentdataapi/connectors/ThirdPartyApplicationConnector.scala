93f6f421bfb6e872fb80814484c1561e4d605c6c
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2024-07-17 07:43:08 +0100

    APIS-7208 - Upgrade to java 21, httpclientv2, bootstrap 9

--- app/uk/gov/hmrc/apiplatformenvironmentdataapi/connectors/ThirdPartyApplicationConnector.scala
+++ app/uk/gov/hmrc/apiplatformenvironmentdataapi/connectors/ThirdPartyApplicationConnector.scala
@@ -20,16 +20,18 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
 
 import uk.gov.hmrc.apiplatform.modules.applications.core.domain.models.ApplicationResponse
 import uk.gov.hmrc.apiplatform.modules.common.domain.models.ClientId
 
 @Singleton
-class ThirdPartyApplicationConnector @Inject() (http: HttpClient, config: ThirdPartyApplicationConnector.Config)(implicit ec: ExecutionContext) {
+class ThirdPartyApplicationConnector @Inject() (http: HttpClientV2, config: ThirdPartyApplicationConnector.Config)(implicit ec: ExecutionContext) {
 
   def getApplicationByClientId(clientId: ClientId)(implicit hc: HeaderCarrier): Future[Option[ApplicationResponse]] = {
-    http.GET[Option[ApplicationResponse]](url = s"${config.serviceBaseUrl}/application", queryParams = Seq("clientId" -> clientId.value))
+    http.get(url"${config.serviceBaseUrl}/application?clientId=${clientId.value}")
+      .execute[Option[ApplicationResponse]]
   }
 }
 
