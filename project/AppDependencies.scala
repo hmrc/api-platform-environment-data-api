c89b94bb8543458beadb31b0edaaf5a319f33fdd
John Green <john.green@digital.hmrc.gov.uk>
2026-08-20 09:12:04 +0100

    API-9372: Upgrade libraries to Provided scope

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,23 +6,26 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
-  private val apiDomainVersion = "1.4.0"
-  private val appDomainVersion = "1.1.0"
+  private val bootstrapVersion    = "10.8.0"
+  private val commonDomainVersion = "1.4.0"
+  private val apiDomainVersion    = "1.8.0"
+  private val appDomainVersion    = "1.6.0"
   private val mockitoScalaVersion = "2.0.0"
 
   val compile = Seq(
     "uk.gov.hmrc"    %% "bootstrap-backend-play-30"         % bootstrapVersion,
+    "uk.gov.hmrc"    %% "api-platform-common-domain"        % commonDomainVersion,
     "uk.gov.hmrc"    %% "api-platform-application-domain"   % appDomainVersion,
     "uk.gov.hmrc"    %% "api-platform-api-domain"           % apiDomainVersion,
-    "uk.gov.hmrc"    %% "internal-auth-client-play-30"      % "4.3.0"
+    "uk.gov.hmrc"    %% "internal-auth-client-play-30"      % "4.4.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"    %% "bootstrap-test-play-30"                     % bootstrapVersion,
     "org.mockito"    %% "mockito-scala-scalatest"                    % mockitoScalaVersion,
+    "uk.gov.hmrc"    %% "api-platform-common-domain-fixtures"        % commonDomainVersion,
     "uk.gov.hmrc"    %% "api-platform-application-domain-fixtures"   % appDomainVersion
-    
+
   ).map(_ % Test)
 
   val it = Seq.empty
